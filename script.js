060dc19a96d4891f30ada788008b0ed1e0e60649
Mohcine Otmane <littlesandflea@gmail.com>
2024-03-20 04:55:38 +0000

    ..

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 let display = document.getElementById("disp");
+let btn = document.querySelectorAll(".btn");
+let output = "1";
 
-display.innerText = "1+1=1000000";
+btn.forEach((bt) =>{
+    bt.onclick = function() {
+        output += String(bt.innerText);
+        console.log(output);
+        display.innerText = output;
+    }
+    
+});
+
