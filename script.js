436e8417cfde669a5ca5aeaf4661f3fe62f9dcf8
Mohcine Otmane <littlesandflea@gmail.com>
2024-03-20 20:00:59 +0000

    ..

--- script.js
+++ script.js
@@ -1,28 +1,74 @@
 let display = document.getElementById("disp");
 let btn = document.querySelectorAll(".btn");
-let clear = document.getElementById("clear");
+let clr = document.getElementById("clear");
+let equal = document.getElementById("equal");
+let clrA = document.getElementById("clear-all");
+
+var equal_sound = new Audio('assets/equal.mp3');
+var button_sound = new Audio('assets/button.mp3');
+
+
+
 
 let output = "";
+let opera = "";
 
 btn.forEach((bt) =>{
     bt.onclick = function() {
         output += String(bt.innerText);
         console.log(output);
-        display.innerText = output;
+        display.innerText = output; 
+        if(bt.innerText!="="){
+            button_sound.play();
+        }  
     }
 });
 
-clear.onclick = function() {
-    display.innerText = clear(output);
-    
-} ;
+clr.onclick = function() {
+    output = clear(output);
+    display.innerText = output;
+};
+
+clrA.onclick = function() {
+    output = "";
+    display.innerText = output;
+};
+
+
+equal.onclick = function() {
+    let a=0;
+    let b=0;
+    equal_sound.play();
+    for(let i=0;i<output.length;i++){
+        if(output[i]=="+" || output[i]=="-" || output[i]=="×" || output[i]=="÷"){
+            opera = output[i];
+            a = parseFloat(output.slice(0,i));
+            b = parseFloat(output.slice(i+1,output.length));
+            console.log(a);
+            console.log(b);
+            if(opera=="+"){
+                output = String(add(a,b));
+            }
+            if(opera=="-"){
+                output = String(substract(a,b));
+            }
+            if(opera=="×"){
+                output = String(multiply(a,b));
+            }
+            if(opera=="÷"){
+                output = String(divide(a,b));
+            }
+            display.innerText = output;
+        }
+    }
+}
 
 function manageInput() {
 
 }
 
 function clear(output) {
-    output = output.slice(0,-1);
+    return output.slice(0,-1);
 }
 
 function clearAll() {
